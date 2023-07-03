af5833103f7e5bb8562c2e5a7901bbaab516ed7c
Katerina <Kateryna.e.nikitina@gmail.com>
2023-07-02 21:14:37 -0600

    styles added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,99 @@
-export const App = () => {
-  return (
-    <div
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+
+// import ListItem from './ListItem/ListItem';
+
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount () {
+    const contacts = localStorage.getItem('contact');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.state.contacts !== prevState.contact) {
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  onFormSubmit = data => {
+    console.log(data);
+    const isExistingContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+
+    if (isExistingContact) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+
+    const contact = { id: nanoid(), ...data };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  onFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  handleClick = data => {
+    console.log(data);
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== data),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
+        // height: '100vh',
+        width: '460px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '24px',
+        padding: '24px',
+        boxShadow: `0px 1px 1px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+        1px 4px 6px rgba(0, 0, 0, 0.16)`,
+      backgroundColor: '#eee',
+      borderRadius: '15px',
         color: '#010101'
       }}
-    >
-      React homework template
-    </div>
-  );
-};
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.onFormSubmit} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.onFilterChange} />
+        <ContactList contacts={filteredContacts} onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
+
+export default App;
